7096ae22a3fa3f78bef495d0fbe8c179ccf6d610
mingu <alsrn7590@naver.com>
2023-02-05 14:04:15 +0900

    Initial commit

---
+++ src/main/java/com/project/runningcrew/RunningcrewApplication.java
@@ -0,0 +1,13 @@
+package com.project.runningcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningcrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningcrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/runningcrew/RunningcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.runningcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningcrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
