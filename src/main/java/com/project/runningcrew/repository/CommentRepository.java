f2192ade12356dda71cbfaad48ea04754816a603
sheisalice606 <1010qazs2@gmail.com>
2023-02-07 20:29:39 +0900

    feat : RunningNoticeRepository 클래스 작성

--- src/main/java/com/project/runningcrew/repository/CommentRepository.java
+++ src/main/java/com/project/runningcrew/repository/CommentRepository.java
@@ -9,30 +9,22 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("select c from Comment c where c.board.id = :boardId")
-    List<Comment> findAllByBoardId(@Param("boardId") Long boardId);
     /**
      * 특정 Board 의 모든 Comment 를 가져오는 메소드다.
+     * @param boardId
+     * @return List of Comment
      */
+    @Query("select c from Comment c where c.board.id = :boardId")
+    List<Comment> findAllByBoardId(@Param("boardId") Long boardId);
 
-    @Query("select c from Comment c where c.member.id = :memberId")
-    List<Comment> findAllByMemberId(@Param("memberId") Long memberId);
+
     /**
      * 특정 Member 의 모든 Comment 를 가져오는 메소드다.
+     * @param memberId
+     * @return List of Comment
      */
-
-
-    /**
-     *
-     * 주요 사용 기능
-     *
-     * save
-     *
-     * delete
-     *
-     * findById
-     *
-     */
+    @Query("select c from Comment c where c.member.id = :memberId")
+    List<Comment> findAllByMemberId(@Param("memberId") Long memberId);
 
 
 
