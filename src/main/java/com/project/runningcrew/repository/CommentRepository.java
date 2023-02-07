d72abbb3ee6536eef7cc995d0b3e5683dbf280ba
sheisalice606 <1010qazs2@gmail.com>
2023-02-07 18:14:16 +0900

    CommentRepository 인터페이스 생성

--- src/main/java/com/project/runningcrew/repository/CommentRepository.java
+++ src/main/java/com/project/runningcrew/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.project.runningcrew.repository;
 
-public class CommentRepository {
+import com.project.runningcrew.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
 }
--- src/main/java/com/project/runningcrew/repository/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.project.runningcrew.repository;
-
-public class UserRepository {
-}
