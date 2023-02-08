de8d657ccc4e35eb32c68ccb9a8da2f230859e33
mingu <alsrn7590@naver.com>
2023-02-08 15:50:48 +0900

    style : CrewRunningRecordRepository.java 코드 포맷 수정
    띄어쓰기 수정, 참조하지 않는 import 삭제

--- src/main/java/com/project/runningcrew/repository/runningrecords/PersonalRunningRecordRepository.java
+++ src/main/java/com/project/runningcrew/repository/runningrecords/PersonalRunningRecordRepository.java
@@ -3,17 +3,12 @@ package com.project.runningcrew.repository.runningrecords;
 import com.project.runningcrew.entity.runningrecords.PersonalRunningRecord;
 import com.project.runningcrew.entity.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface PersonalRunningRecordRepository extends JpaRepository<PersonalRunningRecord, Long> {
-
-
 
     List<PersonalRunningRecord> findAllByUser(User user);
 
