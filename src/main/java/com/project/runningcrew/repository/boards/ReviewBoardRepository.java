a006a72cc460fa11b09b096a81e7fdf79c75174e
sheisalice606 <1010qazs2@gmail.com>
2023-02-13 20:58:49 +0900

    refactor : ReviewBoardRepositoryTest 수정

--- src/main/java/com/project/runningcrew/repository/boards/ReviewBoardRepository.java
+++ src/main/java/com/project/runningcrew/repository/boards/ReviewBoardRepository.java
@@ -1,5 +1,6 @@
 package com.project.runningcrew.repository.boards;
 
+import com.project.runningcrew.entity.Crew;
 import com.project.runningcrew.entity.boards.FreeBoard;
 import com.project.runningcrew.entity.boards.ReviewBoard;
 import org.springframework.data.domain.Page;
@@ -7,16 +8,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
 
 public interface ReviewBoardRepository extends JpaRepository<ReviewBoard, Long> {
 
     /**
+     * 특정 Crew 의 리뷰게시판 목록 paging 적용 출력
      * @param pageable
      * @return page of ReviewBoard
-     * 리뷰게시판 전체 게시물 출력에 paging 을 적용한다. default size = 15
      */
-    @Query("select rb from ReviewBoard rb")
-    Slice<ReviewBoard> findReviewBoardAll(Pageable pageable);
+    @Query("select rb from ReviewBoard rb where rb.member.crew = :crew")
+    Slice<ReviewBoard> findReviewBoardByCrew(@Param("crew") Crew crew, Pageable pageable);
 
 }
