951d1ccf5604d828391b8de76938d23d203fa22d
sheisalice606 <1010qazs2@gmail.com>
2023-02-13 20:38:41 +0900

    feat : FreeBoardRepositoryTest 작성

--- src/main/java/com/project/runningcrew/repository/boards/FreeBoardRepository.java
+++ src/main/java/com/project/runningcrew/repository/boards/FreeBoardRepository.java
@@ -1,11 +1,13 @@
 package com.project.runningcrew.repository.boards;
 
+import com.project.runningcrew.entity.Crew;
 import com.project.runningcrew.entity.boards.FreeBoard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Repository;
 
@@ -14,11 +16,11 @@ import java.util.List;
 public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
 
     /**
+     * 특정 Crew 의 자유게시판 목록 paging 적용 출력
      * @param pageable
      * @return page of FreeBoard
-     * 자유게시판 전체 게시물 출력에 paging 을 적용한다.
      */
-    @Query("select fb from FreeBoard fb")
-    Slice<FreeBoard> findFreeBoardAll(Pageable pageable);
+    @Query("select fb from FreeBoard fb where fb.member.crew = :crew")
+    Slice<FreeBoard> findFreeBoardByCrew(@Param("crew") Crew crew, Pageable pageable);
 
 }
