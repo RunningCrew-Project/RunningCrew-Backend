c91b6625b0eb5b1233dd364ee43d984ca436c512
mingu <alsrn7590@naver.com>
2023-02-15 02:45:43 +0900

    refactor : Crew 의 dongArea 필드로의 변경으로 인한 테스트 리팩토링

--- src/test/java/com/project/runningcrew/repository/CrewRepositoryTest.java
+++ src/test/java/com/project/runningcrew/repository/CrewRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.project.runningcrew.repository;
 
 import com.project.runningcrew.entity.Crew;
+import com.project.runningcrew.entity.areas.DongArea;
+import com.project.runningcrew.entity.areas.GuArea;
+import com.project.runningcrew.entity.areas.SidoArea;
+import com.project.runningcrew.repository.areas.DongAreaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +17,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -22,22 +25,33 @@ class CrewRepositoryTest {
     @Autowired
     CrewRepository crewRepository;
 
+    @Autowired
+    DongAreaRepository dongAreaRepository;
+
+    @Autowired
+    TestEntityFactory testEntityFactory;
+
     @BeforeEach
     public void saveTestCrews() {
+        SidoArea sidoArea = testEntityFactory.getSidoArea(1);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 1);
+        DongArea dongArea1 = testEntityFactory.getDongArea(guArea, 1);
+        DongArea dongArea2 = testEntityFactory.getDongArea(guArea, 2);
+
         for (int i = 0; i < 25; i++) {
             Crew crew = Crew.builder().name("crew"+i)
-                    .location("location1")
                     .introduction("introduction"+i)
                     .crewImgUrl("crewImageUrl"+i)
+                    .dongArea(dongArea1)
                     .build();
             crewRepository.save(crew);
         }
 
         for (int i = 25; i < 50; i++) {
             Crew crew = Crew.builder().name("crew"+i)
-                    .location("location2")
                     .introduction("introduction"+i)
                     .crewImgUrl("crewImageUrl"+i)
+                    .dongArea(dongArea2)
                     .build();
             crewRepository.save(crew);
         }
@@ -46,10 +60,13 @@ class CrewRepositoryTest {
     @Test
     public void saveTest() {
         //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
         Crew crew = Crew.builder().name("crew")
-                .location("location")
                 .introduction("introduction")
                 .crewImgUrl("crewImageUrl")
+                .dongArea(dongArea)
                 .build();
 
         ///when
@@ -62,14 +79,17 @@ class CrewRepositoryTest {
     @Test
     public void findByIdTest() {
         //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
         Crew crew = Crew.builder().name("crew")
-                .location("location")
                 .introduction("introduction")
                 .crewImgUrl("crewImageUrl")
+                .dongArea(dongArea)
                 .build();
+        crewRepository.save(crew);
 
         ///when
-        crewRepository.save(crew);
         Optional<Crew> optCrew = crewRepository.findById(crew.getId());
 
         //then
@@ -80,14 +100,18 @@ class CrewRepositoryTest {
     @Test
     public void deleteTest() {
         //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
         Crew crew = Crew.builder().name("crew")
-                .location("location")
                 .introduction("introduction")
                 .crewImgUrl("crewImageUrl")
+                .dongArea(dongArea)
                 .build();
+        crewRepository.save(crew);
+
 
         ///when
-        Crew savedCrew = crewRepository.save(crew);
         crewRepository.delete(crew);
 
         //then
@@ -98,25 +122,25 @@ class CrewRepositoryTest {
     @Test
     public void findAllByNameOrIntroductionOrLocationTest() {
         //given
-        String keyword = "duction1";
+        String keyword = "ng1";
 
         ///when
-        List<Crew> crewList = crewRepository.findAllByNameOrIntroductionOrLocation(keyword);
+        List<Crew> crewList = crewRepository.findAllByNameOrIntroductionOrArea(keyword);
 
         //then
-        assertThat(crewList.size()).isEqualTo(11);
+        assertThat(crewList.size()).isEqualTo(25);
     }
 
     @Test
     public void countAllByNameOrIntroductionOrLocationTest() {
         //given
-        String keyword = "duction1";
+        String keyword = "ng1";
 
         ///when
-        Long count = crewRepository.countAllByNameOrIntroductionOrLocation(keyword);
+        Long count = crewRepository.countAllByNameOrIntroductionOrArea(keyword);
 
         //then
-        assertThat(count).isEqualTo(11L);
+        assertThat(count).isEqualTo(25L);
     }
 
     @Test
@@ -124,10 +148,10 @@ class CrewRepositoryTest {
         //given
         int size = 15;
         PageRequest pageRequest = PageRequest.of(0, size);
-        String keyword = "duction1";
+        String keyword = "ng1";
 
         ///when
-        Slice<Crew> slice = crewRepository.findByNameOrIntroductionOrLocation(pageRequest, keyword);
+        Slice<Crew> slice = crewRepository.findByNameOrIntroductionOrArea(pageRequest, keyword);
 
         //then
         assertThat(slice.getSize()).isEqualTo(size);
@@ -137,15 +161,17 @@ class CrewRepositoryTest {
     public void findRandomByLocationTest() {
         //given
         int maxNum = 5;
-        String location = "location1";
+        String dongAreaName = "dong1";
+        Optional<DongArea> optDongArea = dongAreaRepository.findByName(dongAreaName);
 
         ///when
-        List<Crew> randomCrewList = crewRepository.findRandomByLocation(location, maxNum);
+        Long dongAreaId = optDongArea.get().getId();
+        List<Crew> randomCrewList = crewRepository.findRandomByDongAreaId(dongAreaId, maxNum);
 
         //then
         assertThat(randomCrewList.size()).isSameAs(maxNum);
         for (Crew crew : randomCrewList) {
-            assertThat(crew.getLocation()).isEqualTo(location);
+            assertThat(crew.getDongArea()).isEqualTo(optDongArea.get());
         }
     }
 
