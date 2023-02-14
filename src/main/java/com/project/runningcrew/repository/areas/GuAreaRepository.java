154dab9be73cc7d4b8175da4d65e7fd8146df8d6
mingu <alsrn7590@naver.com>
2023-02-15 02:10:07 +0900

    feat : DongAreaRepository 에 findByName 메서드 추가 및 주석 추가

--- src/main/java/com/project/runningcrew/repository/areas/GuAreaRepository.java
+++ src/main/java/com/project/runningcrew/repository/areas/GuAreaRepository.java
@@ -10,6 +10,11 @@ import java.util.List;
 @Repository
 public interface GuAreaRepository extends JpaRepository<GuArea, Long> {
 
+    /**
+     * 특정 SidoArea 를 가진 모든 GuArea 들을 이름순으로 오름차순 정리하여 반환
+     * @param sidoArea
+     * @return 특정 SidoArea 를 가진 모든 GuArea 들이 담긴 name 순으로 오름차순 정렬된 list
+     */
     List<GuArea> findAllBySidoAreaOrderByNameAsc(SidoArea sidoArea);
 
 }
--- src/main/java/com/project/runningcrew/repository/areas/SidoAreaRepository.java
+++ src/main/java/com/project/runningcrew/repository/areas/SidoAreaRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 @Repository
 public interface SidoAreaRepository extends JpaRepository<SidoArea, Long> {
 
+    /**
+     * 모든 SidoArea 들을 이름순으로 오름차순 정리하여 반환
+     * @return 모든 SidoArea 들이 담긴 name 순으로 오름차순 정렬된 list
+     */
     List<SidoArea> findAllByOrderByNameAsc();
 
 }
