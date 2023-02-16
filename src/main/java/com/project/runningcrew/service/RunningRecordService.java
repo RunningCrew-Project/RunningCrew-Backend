49d2eb1fde5baecd9b05a38753b11948aeefb050
mingu <alsrn7590@naver.com>
2023-02-17 00:24:00 +0900

    feat : RunningRecordService 생성

--- src/main/java/com/project/runningcrew/service/RunningRecordService.java
+++ src/main/java/com/project/runningcrew/service/RunningRecordService.java
@@ -5,6 +5,7 @@ import com.project.runningcrew.entity.users.User;
 import com.project.runningcrew.exception.RunningRecordNotFoundException;
 import com.project.runningcrew.repository.runningrecords.RunningRecordRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -23,9 +24,10 @@ public class RunningRecordService {
     private final RunningRecordRepository runningRecordRepository;
 
     /**
-     *
-     * @param runningRecordId
-     * @return
+     * 입력받은 id 를 가진 RunningRecord 를 찾아 반환한다. 없다면 RunningRecordNotFoundException 를 throw 한다.
+     * @param runningRecordId 찾는 RunningRecord 의 id
+     * @return 입력받은 id 를 가진 RunningRecord 를 반환한다.
+     * @throws RunningRecordNotFoundException 입력 받은 id 를 가진 RunningRecord 가 없을때
      */
     public RunningRecord findById(Long runningRecordId) {
         return runningRecordRepository.findById(runningRecordId)
@@ -33,32 +35,33 @@ public class RunningRecordService {
     }
 
     /**
-     *
-     * @param runningRecord
-     * @return
+     * 입력받은 RunningRecord 를 저장하고, RunningRecord 에 부여된 id 를 반환한다.
+     * @param runningRecord 저장할 RunningRecord
+     * @return RunningRecord 에 부여된 id
      */
     @Transactional
-    public RunningRecord saveRunningRecord(RunningRecord runningRecord) {
-        return runningRecordRepository.save(runningRecord);
+    public Long saveRunningRecord(RunningRecord runningRecord) {
+        return runningRecordRepository.save(runningRecord).getId();
     }
 
 
     /**
-     *
-     * @param user
+     * 입력받은 User 의 RunningRecord 의 Slice
+     * @param user 찾는 RunningRecord 의 User
      * @param pageable
-     * @return
+     * @return RunningRecord 의 Slice
      */
     public Slice<RunningRecord> findByUser(User user, Pageable pageable) {
         return runningRecordRepository.findByUser(user, pageable);
     }
 
     /**
-     *
-     * @param localDate
-     * @return
+     * 입력받은 User 가 LocalDate 에 런닝을 시작한 모든 RunningRecord 를 반환
+     * @param user 찾는 RunningRecord 의 User
+     * @param localDate 런닝을 시작한 날
+     * @return 런닝을 시작한 날이 LocalDate 인 User 의 모든 RunningRecord
      */
-    public List<RunningRecord> getRunningRecordsByDate(User user, LocalDate localDate) {
+    public List<RunningRecord> findAllByUserAndStartDate(User user, LocalDate localDate) {
         LocalDateTime dateTime = LocalDateTime.of(localDate, LocalTime.of(0, 0));
         LocalDateTime nextDateTime = dateTime.plusDays(1);
         return runningRecordRepository.findAllByUserAndStartDateTimes(user, dateTime, nextDateTime);
