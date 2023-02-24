cdb5b59d0082ba4b8f4d65a6e97a4be0d64033a7
mingu <alsrn7590@naver.com>
2023-02-25 03:00:05 +0900

    refactor : RunningRecord 엔티티 location, title 필드 추가, CrewRunningRecord crew, runningNotice 필드 제거

--- src/main/java/com/project/runningcrew/entity/runningrecords/CrewRunningRecord.java
+++ src/main/java/com/project/runningcrew/entity/runningrecords/CrewRunningRecord.java
@@ -16,21 +16,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class CrewRunningRecord extends RunningRecord {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "crew_id")
-    private Crew crew;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "running_notice_id")
-    private RunningNotice runningNotice;
-
     @Builder
-    public CrewRunningRecord(Long id, LocalDateTime startDateTime, double runningDistance,
-                             int runningTime, int runningFace, int calories, String running_detail,
-                             User user,Crew crew, RunningNotice runningNotice) {
-        super(id, startDateTime, runningDistance, runningTime, runningFace, calories, running_detail, user);
-        this.crew = crew;
-        this.runningNotice = runningNotice;
+    public CrewRunningRecord(Long id, String title, LocalDateTime startDateTime, String location,
+                             double runningDistance, int runningTime, int runningFace,
+                             int calories, String running_detail, User user) {
+        super(id, title, startDateTime, location, runningDistance, runningTime, runningFace,
+                calories, running_detail, user);
     }
 
 }
