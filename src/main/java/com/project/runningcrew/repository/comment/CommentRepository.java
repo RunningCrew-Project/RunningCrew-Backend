ccd847fe3e3d37593e450271026d8b3c25ed2eb6
sheisalice606 <1010qazs2@gmail.com>
2023-03-01 21:53:41 +0900

    refactor : CommentService

--- src/main/java/com/project/runningcrew/repository/comment/CommentRepository.java
+++ src/main/java/com/project/runningcrew/repository/comment/CommentRepository.java
@@ -2,6 +2,8 @@ package com.project.runningcrew.repository.comment;
 
 import com.project.runningcrew.entity.comment.Comment;
 import com.project.runningcrew.entity.members.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,4 +18,13 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * @return List of Comment
      */
     List<Comment> findAllByMember(Member member);
+
+
+    /**
+     * 특정 Member 의 모든 Comment 를 가져온다. (페이징 적용)
+     * @param member
+     * @param pageable
+     * @return slice of Comment
+     */
+    Slice<Comment> findAllByMember(Member member, Pageable pageable);
 }
