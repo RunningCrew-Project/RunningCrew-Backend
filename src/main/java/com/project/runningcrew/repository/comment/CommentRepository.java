147221cccf213406468c1bb275f66a1b9ec4450a
sheisalice606 <1010qazs2@gmail.com>
2023-03-01 21:57:20 +0900

    refactor : CommentService

--- src/main/java/com/project/runningcrew/repository/comment/CommentRepository.java
+++ src/main/java/com/project/runningcrew/repository/comment/CommentRepository.java
@@ -11,15 +11,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
-    /**
-     * 특정 Member 의 모든 Comment 를 가져오는 메소드다.
-     * @param member
-     * @return List of Comment
-     */
-    List<Comment> findAllByMember(Member member);
-
-
     /**
      * 특정 Member 의 모든 Comment 를 가져온다. (페이징 적용)
      * @param member
