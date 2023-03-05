80aad3a18ed7f132b41413e1f0e6d11606453110
mingu <alsrn7590@naver.com>
2023-03-05 17:58:27 +0900

    feat : Notification, NotificationTitle, NotificationType 생성

---
+++ src/main/java/com/project/runningcrew/notification/entity/NotificationType.java
@@ -0,0 +1,18 @@
+package com.project.runningcrew.notification.entity;
+
+public enum NotificationType {
+
+    NOTICE_BOARD(NotificationTitle.NEW_NOTICE_BOARD_TITLE),
+    REGULAR_RUNNING_NOTICE(NotificationTitle.NEW_REGULAR_RUNNING_NOTICE_TITLE);
+
+    private String title;
+
+    NotificationType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
