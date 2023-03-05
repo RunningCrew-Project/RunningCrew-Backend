91246077c07f29aacf00123a62073c5be6fb8e43
mingu <alsrn7590@naver.com>
2023-03-05 18:32:32 +0900

    feat : NotificationRepository 생성 및 테스트

---
+++ src/main/java/com/project/runningcrew/notification/repository/NotificationRepository.java
@@ -0,0 +1,23 @@
+package com.project.runningcrew.notification.repository;
+
+import com.project.runningcrew.entity.users.User;
+import com.project.runningcrew.notification.entity.Notification;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    /**
+     * 특정 User 의 Notification 을 페이징하여 반환한다.
+     *
+     * @param user
+     * @param pageable
+     * @return 특정 User 의 Notification
+     */
+    Slice<Notification> findByUser(@Param("user") User user, Pageable pageable);
+
+}
---
+++ src/test/java/com/project/runningcrew/notification/repository/NotificationRepositoryTest.java
@@ -0,0 +1,145 @@
+package com.project.runningcrew.notification.repository;
+
+import com.project.runningcrew.entity.Crew;
+import com.project.runningcrew.entity.areas.DongArea;
+import com.project.runningcrew.entity.areas.GuArea;
+import com.project.runningcrew.entity.areas.SidoArea;
+import com.project.runningcrew.entity.boards.NoticeBoard;
+import com.project.runningcrew.entity.members.Member;
+import com.project.runningcrew.entity.runningnotices.RunningNotice;
+import com.project.runningcrew.entity.users.User;
+import com.project.runningcrew.notification.entity.Notification;
+import com.project.runningcrew.repository.TestEntityFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class NotificationRepositoryTest {
+
+    @Autowired
+    NotificationRepository notificationRepository;
+
+    @Autowired
+    TestEntityFactory testEntityFactory;
+
+
+    @DisplayName("공지글 Notification 저장 테스트")
+    @Test
+    public void saveTest1() {
+        //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
+        User user = testEntityFactory.getUser(dongArea, 0);
+        Crew crew = testEntityFactory.getCrew(dongArea, 0);
+        Member member = testEntityFactory.getMember(user, crew);
+        NoticeBoard noticeBoard = testEntityFactory.getNoticeBoard(member, 0);
+        Notification notification = Notification.createNoticeBoardNotification(user, crew, noticeBoard);
+
+        ///when
+        Notification savedNotification = notificationRepository.save(notification);
+
+        //then
+        assertThat(savedNotification).isEqualTo(notification);
+    }
+
+    @DisplayName("정기런닝 Notification 저장 테스트")
+    @Test
+    public void saveTest2() {
+        //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
+        User user = testEntityFactory.getUser(dongArea, 0);
+        Crew crew = testEntityFactory.getCrew(dongArea, 0);
+        Member member = testEntityFactory.getMember(user, crew);
+        RunningNotice regularRunningNotice = testEntityFactory.getRegularRunningNotice(member, 0);
+        Notification notification = Notification.createRegularRunningNotification(user, crew, regularRunningNotice);
+
+        ///when
+        Notification savedNotification = notificationRepository.save(notification);
+
+        //then
+        assertThat(savedNotification).isEqualTo(notification);
+    }
+
+    @DisplayName("User 의 Notification 페이징하여 반환 첫 페이지 테스트")
+    @Test
+    public void findByUserTest1() {
+        //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
+        User user = testEntityFactory.getUser(dongArea, 0);
+        Crew crew = testEntityFactory.getCrew(dongArea, 0);
+        Member member = testEntityFactory.getMember(user, crew);
+        for (int i = 0; i < 10; i++) {
+            NoticeBoard noticeBoard = testEntityFactory.getNoticeBoard(member, 0);
+            Notification notification = Notification.createNoticeBoardNotification(user, crew, noticeBoard);
+            notificationRepository.save(notification);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            RunningNotice regularRunningNotice = testEntityFactory.getRegularRunningNotice(member, 0);
+            Notification notification = Notification.createRegularRunningNotification(user, crew, regularRunningNotice);
+            notificationRepository.save(notification);
+        }
+
+        ///when
+        PageRequest pageRequest = PageRequest.of(0, 12);
+        Slice<Notification> notifications = notificationRepository.findByUser(user, pageRequest);
+
+        //then
+        assertThat(notifications.getNumber()).isSameAs(0);
+        assertThat(notifications.getSize()).isSameAs(12);
+        assertThat(notifications.getNumberOfElements()).isSameAs(12);
+        assertThat(notifications.hasPrevious()).isFalse();
+        assertThat(notifications.hasNext()).isTrue();
+        assertThat(notifications.isFirst()).isTrue();
+    }
+
+    @DisplayName("User 의 Notification 페이징하여 반환 마지막 페이지 테스트")
+    @Test
+    public void findByUserTest2() {
+        //given
+        SidoArea sidoArea = testEntityFactory.getSidoArea(0);
+        GuArea guArea = testEntityFactory.getGuArea(sidoArea, 0);
+        DongArea dongArea = testEntityFactory.getDongArea(guArea, 0);
+        User user = testEntityFactory.getUser(dongArea, 0);
+        Crew crew = testEntityFactory.getCrew(dongArea, 0);
+        Member member = testEntityFactory.getMember(user, crew);
+        for (int i = 0; i < 10; i++) {
+            NoticeBoard noticeBoard = testEntityFactory.getNoticeBoard(member, 0);
+            Notification notification = Notification.createNoticeBoardNotification(user, crew, noticeBoard);
+            notificationRepository.save(notification);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            RunningNotice regularRunningNotice = testEntityFactory.getRegularRunningNotice(member, 0);
+            Notification notification = Notification.createRegularRunningNotification(user, crew, regularRunningNotice);
+            notificationRepository.save(notification);
+        }
+
+        ///when
+        PageRequest pageRequest = PageRequest.of(1, 12);
+        Slice<Notification> notifications = notificationRepository.findByUser(user, pageRequest);
+
+        //then
+        assertThat(notifications.getNumber()).isSameAs(1);
+        assertThat(notifications.getSize()).isSameAs(12);
+        assertThat(notifications.getNumberOfElements()).isSameAs(8);
+        assertThat(notifications.hasPrevious()).isTrue();
+        assertThat(notifications.hasNext()).isFalse();
+        assertThat(notifications.isLast()).isTrue();
+    }
+
+}
