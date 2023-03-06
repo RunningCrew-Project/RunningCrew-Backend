54da6227981405d540a81a5475b57a9e796a9f32
mingu <alsrn7590@naver.com>
2023-03-06 14:17:59 +0900

    chore : SecurityConfig 생성

---
+++ src/main/java/com/project/runningcrew/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.project.runningcrew.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests().anyRequest().permitAll().and().build();
+    }
+
+}
