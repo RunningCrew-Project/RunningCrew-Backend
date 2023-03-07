00ffc1234c6407e69a28b6bb013fdc2f8bfa7938
sheisalice606 <1010qazs2@gmail.com>
2023-03-07 14:19:10 +0900

    feat : ImageS3ServiceImpl

--- src/main/java/com/project/runningcrew/service/images/AmazonS3Config.java
+++ src/main/java/com/project/runningcrew/service/images/AmazonS3Config.java
@@ -9,13 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AwsS3Config {
+public class AmazonS3Config {
 
-
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${spring.s3.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${spring.s3.secret-key}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
@@ -24,10 +23,10 @@ public class AwsS3Config {
     @Bean
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
                 .build();
     }
 
