11a94c66685438d0ce830be0ae77485d8397736a
mingu <alsrn7590@naver.com>
2023-03-07 23:15:10 +0900

    chore : SecurityConfig, SwaggerConfig 설정 추가
    SecurityConfig : passwordEncoder 생성, csrf, formLogin disable
    SwaggerConfig : 헤더값 적용 기능 추가

--- src/main/java/com/project/runningcrew/config/SwaggerConfig.java
+++ src/main/java/com/project/runningcrew/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.project.runningcrew.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +25,11 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Running Crew API")
                         .description("Running Crew 프로젝트 API 명세서 입니다.")
-                        .version("v1"));
+                        .version("v1"))
+                .components(new Components().addSecuritySchemes("Bearer-Key",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer").bearerFormat("JWT"))
+                );
     }
-
 
 }
