b37c82eef68d0089610538cd9e606a3792ed6da7
mingu <alsrn7590@naver.com>
2023-03-08 14:44:12 +0900

    feat : CrewController 의 createCrew, findCrewsByKeyword, findRandomCrewsByGuArea 추가

--- src/test/java/com/project/runningcrew/crew/controller/CrewControllerTest.java
+++ src/test/java/com/project/runningcrew/crew/controller/CrewControllerTest.java
@@ -1,27 +1,38 @@
-package com.project.runningcrew.crew;
+package com.project.runningcrew.crew.controller;
 
 import com.project.runningcrew.area.entity.DongArea;
+import com.project.runningcrew.area.entity.GuArea;
+import com.project.runningcrew.area.service.DongAreaService;
+import com.project.runningcrew.area.service.GuAreaService;
 import com.project.runningcrew.crew.controller.CrewController;
+import com.project.runningcrew.crew.dto.CreateCrewRequest;
 import com.project.runningcrew.crew.entity.Crew;
 import com.project.runningcrew.crew.service.CrewService;
+import com.project.runningcrew.exception.notFound.CrewNotFoundException;
 import com.project.runningcrew.member.service.MemberService;
+import com.project.runningcrew.user.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @MockBean(JpaMetamodelMappingContext.class)
-@WebMvcTest(CrewController.class)
+@WebMvcTest(controllers = CrewController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class CrewControllerTest {
 
     @MockBean
@@ -31,12 +42,17 @@ class CrewControllerTest {
     private MemberService memberService;
 
     @MockBean
-    DongArea dongArea;
+    private DongAreaService dongAreaService;
+
+    @MockBean
+    private GuAreaService guAreaService;
+
+    @MockBean
+    private DongArea dongArea;
 
     @Autowired
     private MockMvc mockMvc;
 
-    @WithMockUser
     @DisplayName("크루 가져오기 성공 테스트")
     @Test
     public void getCrewTest1() throws Exception {
@@ -71,7 +87,44 @@ class CrewControllerTest {
                 .andExpect(jsonPath("$.crewImgUrl").value(crewImgUrl))
                 .andExpect(jsonPath("$.dong").value(dongFullName))
                 .andExpect(jsonPath("$.memberCount").value(memberCount));
-
     }
+
+    @WithMockUser
+    @DisplayName("크루 가져오기 성공 테스트")
+    @Test
+    public void getCrewTest2() throws Exception {
+        //given
+        Long crewId = 1L;
+        when(crewService.findById(crewId)).thenThrow(CrewNotFoundException.class);
+
+        ///when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/crews/%d", crewId)));
+
+        //then
+        resultActions.andExpect(status().isNotFound());
+    }
+
+//    @DisplayName("크루 생성하기 테스트")
+//    @Test
+//    public void createCrewTest(@Mock GuArea guArea, @Mock MultipartFile multipartFile) throws Exception{
+//        //given
+//        Long dongId = 1L;
+//        Long crewId = 2L;
+//        DongArea dongArea = new DongArea(dongId, "전농동", guArea);
+//        CreateCrewRequest createCrewRequest = new CreateCrewRequest("crew", "introduction",
+//                dongId, multipartFile);
+//        when(dongAreaService.findById(dongId)).thenReturn(dongArea);
+//        when(crewService.saveCrew(any(), any(), multipartFile)).thenReturn(crewId);
+//
+//        ///when
+//        ResultActions resultActions = mockMvc.perform(
+//                MockMvcRequestBuilders.post("/api/crews")
+//                        .content();
+//
+//        //then
+//        resultActions.andExpect(status().isCreated())
+//                .andExpect(header().string("Location", ""));
+//    }
 
 }
