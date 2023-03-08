5a82128258324c0fe5c7a4e287e6f5d3fd7ad19c
sheisalice606 <1010qazs2@gmail.com>
2023-03-08 19:37:08 +0900

    feat : s3

--- src/main/java/com/project/runningcrew/image/ImageS3ServiceImpl.java
+++ src/main/java/com/project/runningcrew/image/ImageS3ServiceImpl.java
@@ -21,9 +21,6 @@ public class ImageS3ServiceImpl implements ImageService{
     @Value("${spring.s3.bucket}")
     private String bucketName;
 
-    private String DOWNLOAD_PATH = "C:\\Users\\alsrn\\Desktop\\test";
-    private String SEPARATOR = File.separator;
-
     private final AmazonS3 amazonS3;
     private final AmazonS3Client amazonS3Client;
 
@@ -34,24 +31,20 @@ public class ImageS3ServiceImpl implements ImageService{
             throw new ImageFileCreationException();
         }
 
-        String fileName = UUID.randomUUID().toString();
-        String extension = multipartFile.getOriginalFilename().split("\\.")[1];
-        File uploadFile = new File(DOWNLOAD_PATH + SEPARATOR + dirName + SEPARATOR
-                + fileName + "." + extension);
-
+        String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
 
         ObjectMetadata obj = new ObjectMetadata();
         obj.setContentLength(multipartFile.getSize());
         obj.setContentType(multipartFile.getContentType());
 
         try {
-            amazonS3.putObject(bucketName, fileName, multipartFile.getInputStream(), obj);
+            amazonS3.putObject(bucketName, s3FileName, multipartFile.getInputStream(), obj);
         } catch (Exception e) {
             // 이미지 업로드 에러 발생
             throw new S3UploadException();
         }
 
-        return amazonS3.getUrl(bucketName, fileName).toString();
+        return amazonS3.getUrl(bucketName, s3FileName).toString();
     }
 
     @Override
---
+++ src/test/java/com/project/runningcrew/image/ImageS3ServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.project.runningcrew.image;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageS3ServiceImplTest {
+
+}
