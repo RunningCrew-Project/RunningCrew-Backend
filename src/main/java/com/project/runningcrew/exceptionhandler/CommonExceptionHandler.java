70902097d6d028aa37877b35091ff5cc1cff632c
mingu <alsrn7590@naver.com>
2023-03-10 12:53:51 +0900

    feat : JwtExpiredExceptionHandler, JwtVerificationExceptionHandler 추가

--- src/main/java/com/project/runningcrew/exceptionhandler/CommonExceptionHandler.java
+++ src/main/java/com/project/runningcrew/exceptionhandler/CommonExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.project.runningcrew.exceptionhandler;
 
+import com.project.runningcrew.exception.jwt.JwtExpiredException;
+import com.project.runningcrew.exception.jwt.JwtVerificationException;
 import com.project.runningcrew.exception.notFound.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,5 +28,38 @@ public class CommonExceptionHandler {
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    /**
+     * JwtExpiredException 이 발생하면, ErrorResponse 와 HttpStatus.UNAUTHORIZED 를 담은 ResponseEntity 를 반환
+     *
+     * @param e
+     * @return ErrorResponse 와 HttpStatus.UNAUTHORIZED 를 담은 ResponseEntity
+     */
+    @ExceptionHandler(JwtExpiredException.class)
+    public ResponseEntity<ErrorResponse> JwtExpiredExceptionHandler(JwtExpiredException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .messages(e.getMessage())
+                .errors(Map.of())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    /**
+     * JwtVerificationException 이 발생하면, ErrorResponse 와 HttpStatus.FORBIDDEN 를 담은 ResponseEntity 를 반환
+     *
+     * @param e
+     * @return ErrorResponse 와 HttpStatus.FORBIDDEN 를 담은 ResponseEntity
+     */
+    @ExceptionHandler(JwtVerificationException.class)
+    public ResponseEntity<ErrorResponse> JwtVerificationExceptionHandler(JwtVerificationException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.FORBIDDEN.value())
+                .messages(e.getMessage())
+                .errors(Map.of())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
 
 }
