dfad9ac4bd78ab628be7ecd2700648de555e60f2
mingu <alsrn7590@naver.com>
2023-03-15 13:52:57 +0900

    feat : BoardImageService 의 findById 추가 및 테스트

--- src/main/java/com/project/runningcrew/resourceimage/service/BoardImageService.java
+++ src/main/java/com/project/runningcrew/resourceimage/service/BoardImageService.java
@@ -1,6 +1,7 @@
 package com.project.runningcrew.resourceimage.service;
 
 import com.project.runningcrew.board.entity.Board;
+import com.project.runningcrew.exception.notFound.ImageNotFoundException;
 import com.project.runningcrew.resourceimage.entity.BoardImage;
 import com.project.runningcrew.resourceimage.repository.BoardImageRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,18 @@ public class BoardImageService {
 
     private final BoardImageRepository boardImageRepository;
     private final String defaultImageUrl = "defaultImageUrl";
+
+
+    /**
+     * 입력받은 id 를 가진 BoardImage 를 찾아 반환한다. 없다면 ImageNotFoundException 을 throw 한다.
+     *
+     * @param boardImageId 찾는 BoardImage 의 id
+     * @return boardImageId 를 id 로 가지는 BoardImage
+     * @throws ImageNotFoundException boardImageId 에 해당하는 BoardImage 가 존재하지 않을 때
+     */
+    public BoardImage findById(Long boardImageId) {
+        return boardImageRepository.findById(boardImageId).orElseThrow(ImageNotFoundException::new);
+    }
 
     /**
      * Board 에 포함된 모든 BoardImage 를 반환한다.
@@ -43,7 +56,7 @@ public class BoardImageService {
         List<BoardImage> images = boardImageRepository.findImagesByBoardIds(boardIds);
         for (Long boardId : boardIds) {
             BoardImage first = images.stream()
-                    .filter(image -> image.getBoard().getId() == boardId)
+                    .filter(image -> image.getBoard().getId().equals(boardId))
                     .findFirst().orElseGet(() -> new BoardImage(defaultImageUrl, null));
             maps.put(boardId, first);
         }
