e23a3510d0ba80448c386c22af6d99a3775c4619
mingu <alsrn7590@naver.com>
2023-03-21 01:02:36 +0900

    feat : ApplyResponse 생성, RunningNoticeValidResponse 를 ReadyResponse 로 수정, RunningNoticeController 의 checkRunningApply 추가, updateRunningNotice 수정

---
+++ src/main/java/com/project/runningcrew/common/dto/ApplyResponse.java
@@ -0,0 +1,14 @@
+package com.project.runningcrew.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplyResponse {
+
+    private String isApply;
+
+}
