168f6c6543165ad6ef62f34f2656e0af3d2f952c
mingu <alsrn7590@naver.com>
2023-04-04 22:16:27 +0900

    refactor : S3Image 관련 예외처리 리팩토링
    S3ImageException 생성
    S3UploadException, S3DeleteException 리팩토링
    CommonExceptionHandler 에 ImageExceptionHandler 추가

--- src/main/java/com/project/runningcrew/exception/s3/S3UploadException.java
+++
@@ -1,7 +0,0 @@
-package com.project.runningcrew.exception.s3;
-
-public class S3UploadException extends RuntimeException {
-    public S3UploadException() {
-        super("S3 버킷에 이미지 업로드 중 문제가 발생했습니다.");
-    }
-}
