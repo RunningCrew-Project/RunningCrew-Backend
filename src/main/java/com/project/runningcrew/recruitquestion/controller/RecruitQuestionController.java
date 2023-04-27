6749990ce3e007f948c9ac304a9a0bf27dd29af3
mingu <alsrn7590@naver.com>
2023-04-27 22:18:16 +0900

    feat : RecruitQuestionsDto 생성, RecruitQuestionController 의 setRecruitQuestions 추가

--- src/main/java/com/project/runningcrew/recruitquestion/controller/RecruitQuestionController.java
+++ src/main/java/com/project/runningcrew/recruitquestion/controller/RecruitQuestionController.java
@@ -9,6 +9,7 @@ import com.project.runningcrew.exception.badinput.CrewJoinQuestionException;
 import com.project.runningcrew.exceptionhandler.ErrorResponse;
 import com.project.runningcrew.member.service.MemberAuthorizationChecker;
 import com.project.runningcrew.recruitquestion.dto.request.CreateRecruitQuestionDto;
+import com.project.runningcrew.recruitquestion.dto.request.RecruitQuestionsDto;
 import com.project.runningcrew.recruitquestion.dto.response.GetExistOfRecruitQuestion;
 import com.project.runningcrew.recruitquestion.dto.response.GetRecruitQuestionDto;
 import com.project.runningcrew.recruitquestion.dto.response.GetRecruitQuestionList;
@@ -51,73 +52,71 @@ public class RecruitQuestionController {
     private String host;
 
 
-
-    @Operation(summary = "가입 질문 하나 생성하기", description = "가입 질문 하나를 생성한다.", security = {@SecurityRequirement(name = "Bearer-Key")})
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "CREATED", content = @Content()),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @PostMapping("/api/crews/{crewId}/recruit-questions")
-    public ResponseEntity<Void> createOneRecruitQuestion(
-            @PathVariable("crewId") Long crewId,
-            @RequestBody @Valid CreateRecruitQuestionDto createRecruitQuestionDto,
-            @Parameter(hidden = true) @CurrentUser User user
-    ) {
-        Crew crew = crewService.findById(crewId);
-        CrewCondition crewCondition = crewConditionService.findByCrew(crew);
-        if (!crewCondition.isJoinQuestion()) {
-            throw new CrewJoinQuestionException();
-        }
-
-        memberAuthorizationChecker.checkLeader(user, crew);
-        //note 요청 user 의 크루 Leader 자격 검증
-
-        RecruitQuestion recruitQuestion = new RecruitQuestion(crew, createRecruitQuestionDto.getQuestion(), createRecruitQuestionDto.getQuestionOffset());
-        Long questionId = recruitQuestionService.saveOneQuestion(recruitQuestion);
-
-        URI uri = UriComponentsBuilder
-                .fromHttpUrl(host)
-                .path("/api/recruit-questions/{id}")
-                .build(questionId);
-
-        return ResponseEntity.created(uri).build();
-    }
-
-
-
-    @Operation(summary = "가입 질문 하나 삭제하기", description = "가입 질문 하나를 삭제한다.", security = {@SecurityRequirement(name = "Bearer-Key")})
-    @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "NO CONTENT", content = @Content()),
-            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    @DeleteMapping("/api/recruit-questions/{recruitQuestionId}")
-    public ResponseEntity<Void> deleteOneRecruitQuestion(
-            @PathVariable("recruitQuestionId") Long recruitQuestionId,
-            @Parameter(hidden = true) @CurrentUser User user
-    ) {
-        RecruitQuestion recruitQuestion = recruitQuestionService.findById(recruitQuestionId);
-        Crew crew = recruitQuestion.getCrew();
-        CrewCondition crewCondition = crewConditionService.findByCrew(crew);
-        if (!crewCondition.isJoinQuestion()) {
-            throw new CrewJoinQuestionException();
-        }
-
-        memberAuthorizationChecker.checkLeader(user, crew);
-        //note 요청 user 의 크루 Leader 자격 검증
-
-        recruitQuestionService.deleteOneQuestion(recruitQuestion);
-        return ResponseEntity.noContent().build();
-    }
+//    @Operation(summary = "가입 질문 하나 생성하기", description = "가입 질문 하나를 생성한다.", security = {@SecurityRequirement(name = "Bearer-Key")})
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "201", description = "CREATED", content = @Content()),
+//            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+//                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+//            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
+//                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+//            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
+//                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+//    })
+//    @PostMapping("/api/crews/{crewId}/recruit-questions")
+//    public ResponseEntity<Void> createOneRecruitQuestion(
+//            @PathVariable("crewId") Long crewId,
+//            @RequestBody @Valid CreateRecruitQuestionDto createRecruitQuestionDto,
+//            @Parameter(hidden = true) @CurrentUser User user
+//    ) {
+//        Crew crew = crewService.findById(crewId);
+//        CrewCondition crewCondition = crewConditionService.findByCrew(crew);
+//        if (!crewCondition.isJoinQuestion()) {
+//            throw new CrewJoinQuestionException();
+//        }
+//
+//        memberAuthorizationChecker.checkLeader(user, crew);
+//        //note 요청 user 의 크루 Leader 자격 검증
+//
+//        RecruitQuestion recruitQuestion = new RecruitQuestion(crew, createRecruitQuestionDto.getQuestion(), createRecruitQuestionDto.getQuestionOffset());
+//        Long questionId = recruitQuestionService.saveOneQuestion(recruitQuestion);
+//
+//        URI uri = UriComponentsBuilder
+//                .fromHttpUrl(host)
+//                .path("/api/recruit-questions/{id}")
+//                .build(questionId);
+//
+//        return ResponseEntity.created(uri).build();
+//    }
+//
+//
+//    @Operation(summary = "가입 질문 하나 삭제하기", description = "가입 질문 하나를 삭제한다.", security = {@SecurityRequirement(name = "Bearer-Key")})
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "204", description = "NO CONTENT", content = @Content()),
+//            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
+//                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+//            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
+//                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+//            @ApiResponse(responseCode = "404", description = "NOT FOUND",
+//                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+//    })
+//    @DeleteMapping("/api/recruit-questions/{recruitQuestionId}")
+//    public ResponseEntity<Void> deleteOneRecruitQuestion(
+//            @PathVariable("recruitQuestionId") Long recruitQuestionId,
+//            @Parameter(hidden = true) @CurrentUser User user
+//    ) {
+//        RecruitQuestion recruitQuestion = recruitQuestionService.findById(recruitQuestionId);
+//        Crew crew = recruitQuestion.getCrew();
+//        CrewCondition crewCondition = crewConditionService.findByCrew(crew);
+//        if (!crewCondition.isJoinQuestion()) {
+//            throw new CrewJoinQuestionException();
+//        }
+//
+//        memberAuthorizationChecker.checkLeader(user, crew);
+//        //note 요청 user 의 크루 Leader 자격 검증
+//
+//        recruitQuestionService.deleteOneQuestion(recruitQuestion);
+//        return ResponseEntity.noContent().build();
+//    }
 
 
     @Operation(summary = "가입 질문 전체 조회하기", description = "가입 질문 전체를 조회한다.", security = {@SecurityRequirement(name = "Bearer-Key")})
@@ -143,6 +142,35 @@ public class RecruitQuestionController {
     }
 
 
+    @Operation(summary = "가입 질문들 설정하기", description = "기존 가입 질문들은 삭제하고, 입력받은 가입 질문들로 새로 설정한다.", security = {@SecurityRequirement(name = "Bearer-Key")})
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "NO CONTENT", content = @Content()),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PutMapping("/api/crew/{crewId}/recruit-questions")
+    public ResponseEntity<Void> setRecruitQuestions(@PathVariable("crewId") Long crewId,
+                                                    @Parameter(hidden = true) @CurrentUser User user,
+                                                    @RequestBody @Valid RecruitQuestionsDto recruitQuestionsDto
+    ) {
+        Crew crew = crewService.findById(crewId);
+        CrewCondition crewCondition = crewConditionService.findByCrew(crew);
+        if (!crewCondition.isJoinQuestion()) {
+            throw new CrewJoinQuestionException();
+        }
+        memberAuthorizationChecker.checkLeader(user, crew);
+
+        List<RecruitQuestion> recruitQuestions = recruitQuestionsDto.getQuestions().stream()
+                .map(r -> new RecruitQuestion(crew, r.getQuestion(), r.getQuestionOffset()))
+                .collect(Collectors.toList());
+
+        recruitQuestionService.setRecruitQuestions(crew, recruitQuestions);
+        return ResponseEntity.noContent().build();
+    }
 
 
     @Operation(summary = "특정 크루의 가입 질문 설정 여부",
@@ -167,16 +195,13 @@ public class RecruitQuestionController {
         Crew crew = crewService.findById(crewId);
         List<RecruitQuestion> questionList = recruitQuestionService.findAllByCrew(crew);
 
-        if(!questionList.isEmpty()) {
+        if (!questionList.isEmpty()) {
             return ResponseEntity.ok(new GetExistOfRecruitQuestion(true));
         } else {
             return ResponseEntity.ok(new GetExistOfRecruitQuestion(false));
         }
 
     }
-
-
-
 
 
 }
