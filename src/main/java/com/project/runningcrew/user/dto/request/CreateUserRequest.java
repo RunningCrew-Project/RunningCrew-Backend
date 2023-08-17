cc5c3458c5f04396871a58350e18feb469cc847d
mingu <alsrn7590@naver.com>
2023-08-17 15:43:05 +0900

    feat : UserRoleService 생성, UserController 의 createUser 에 비밀번후 추가, updateUser, deleteUser 에 권한 로직 추가

--- src/main/java/com/project/runningcrew/user/dto/request/CreateUserRequest.java
+++ src/main/java/com/project/runningcrew/user/dto/request/CreateUserRequest.java
@@ -35,6 +35,10 @@ public class CreateUserRequest {
     @NotBlank(message = "닉네임은 필수 값입니다.")
     private String nickname;
 
+    @Schema(description = "생성 유저 비밀번호 입력", example = "same_password")
+    @NotBlank(message = "비밀번호는 필수 값입니다.")
+    private String password;
+
     @Schema(description = "생성 유저 동네 아이디", example = "1")
     @NotNull(message = "동네 아이디는 필수 값입니다.")
     @Positive(message = "동네 아이디는 1 이상의 수입니다.")
