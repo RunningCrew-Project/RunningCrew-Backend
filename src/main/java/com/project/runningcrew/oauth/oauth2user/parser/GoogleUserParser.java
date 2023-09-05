6991ba0890bdf80c7420fd9015d597fa330988d5
mingu <alsrn7590@naver.com>
2023-09-05 18:54:59 +0900

    feat : soft delete 된 유저 존재 시 삭제 후 재가입 로직 추가

--- src/main/java/com/project/runningcrew/oauth/oauth2user/parser/GoogleUserParser.java
+++ src/main/java/com/project/runningcrew/oauth/oauth2user/parser/GoogleUserParser.java
@@ -1,30 +1,24 @@
 package com.project.runningcrew.oauth.oauth2user.parser;
 
-import com.project.runningcrew.exception.notFound.UserNotFoundException;
 import com.project.runningcrew.exception.notFound.UserRoleNotFoundException;
 import com.project.runningcrew.oauth.OAuth2User;
 import com.project.runningcrew.oauth.dto.request.OauthDto;
 import com.project.runningcrew.oauth.oauth2user.userinfo.GoogleUserInfo;
-import com.project.runningcrew.oauth.oauth2user.userinfo.KakaoUserInfo;
 import com.project.runningcrew.user.entity.LoginType;
 import com.project.runningcrew.user.entity.User;
 import com.project.runningcrew.user.repository.UserRepository;
-import com.project.runningcrew.user.service.UserService;
 import com.project.runningcrew.userrole.entity.Role;
 import com.project.runningcrew.userrole.entity.UserRole;
 import com.project.runningcrew.userrole.repository.UserRoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.parser.ParseException;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.IOException;
 import java.util.Map;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -45,27 +39,33 @@ public class GoogleUserParser {
         GoogleUserInfo googleUserInfo = new GoogleUserInfo(attributes);
         String email = googleUserInfo.getEmail();
 
-        if (!userRepository.existsByEmail(email)) {
-            log.info("입력받은 Email 로 가입된 Google 회원이 없습니다.");
-
-            User user = User.builder()
-                    .email(email)
-                    .login_type(LoginType.GOOGLE)
-                    .build();
-
-            User savedUser = userRepository.save(user);
-            log.info("Google 회원등록, user={}", savedUser);
-
-            UserRole userRole = new UserRole(user, Role.USER);
-            userRoleRepository.save(userRole);
-            log.info("Google 회원 역할등록, userRole={}", userRole);
-
-            return new OAuth2User(savedUser, userRole);
+        Optional<User> optionalUser = userRepository.findByEmailForAdmin(email);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            UserRole userRole = userRoleRepository.findByUserForAdmin(user).orElseThrow(UserRoleNotFoundException::new);
+            if (user.isDeleted()) {
+                userRepository.deleteForAdmin(user);
+                userRoleRepository.deleteForAdmin(userRole);
+            } else {
+                return new OAuth2User(user, userRole);
+            }
         }
 
-        User user = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
-        UserRole userRole = userRoleRepository.findByUser(user).orElseThrow(UserRoleNotFoundException::new);
-        return new OAuth2User(user, userRole);
+        log.info("입력받은 Email 로 가입된 Google 회원이 없습니다.");
+
+        User user = User.builder()
+                .email(email)
+                .login_type(LoginType.GOOGLE)
+                .build();
+
+        User savedUser = userRepository.save(user);
+        log.info("Google 회원등록, user={}", savedUser);
+
+        UserRole userRole = new UserRole(user, Role.USER);
+        userRoleRepository.save(userRole);
+        log.info("Google 회원 역할등록, userRole={}", userRole);
+
+        return new OAuth2User(savedUser, userRole);
     }
 
 
